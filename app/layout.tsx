c95e9f20dd4aa6f788232c7cd608fad293772058
wldnz <wildanofficial32@gmail.com>
2026-01-06 19:20:57 +0700

    init bermalam

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interFont = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"]
+})
+
+export const metadata: Metadata = {
+  title: "Bermalam",
+  description: "Kamu butuh tempat bermalam? Tenang Aja! Bermalam Hadir Untuk Memudahkan Kamu Dalam Memesan Hotel Yang Sedang Kamu Tuju",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${interFont.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
