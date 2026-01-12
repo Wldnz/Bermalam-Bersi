b013a17d59a8bbd5312e03938025e5167d7c1f64
wldnz <wildanofficial32@gmail.com>
2026-01-12 12:33:23 +0700

    progress on dashboard bermalam

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,112 @@
+"use client"
+import BookingIcons from "@/components/Icons/Booking";
+import Navigation from "@/components/Navigation";
 import Image from "next/image";
+import { useRef } from "react";
+
+
+
 
 export default function Home() {
+  const checkInInput = useRef(null)
+  // const checkOutInput = useRef(null)
+
+  function handleClick(){
+
+    if (checkInInput.current && (checkInInput.current as HTMLInputElement).showPicker){
+      (checkInInput.current as HTMLInputElement).showPicker()
+    }
+
+  }
+
   return (
     <>
-      
+      <div className="w-full h-dvh bg-[url(/images/dashboard.png)] bg-no-repeat bg-cover relative">
+
+        {/* black nuansa */}
+
+        <div className="w-full h-full bg-foreground opacity-60"></div>
+
+        <div className="w-full h-full p-6 absolute top-0 lef-0 ">
+          <Navigation />
+
+          <div className="w-ful flex flex-col items-center gap-10 mt-14">
+
+            <div className="w-full flex flex-col items-center gap-3.5 text-background">
+              <h2 className="w-full text-center text-3xl font-bold">Selamat Siang!, Mau Bermalam Dimana Nih?</h2>
+              <p className="text-xl" >Tenang Aja Kok, Kami sudah menyiapkan banyak tempat untuk bermalam</p>
+            </div>
+
+            {/* seaarch bar */}
+            <div className="w-[80%] flex flex-col items-center">
+              <div className="w-full flex gap-2.5">
+                <div className="h-7 flex items-center self-end gap-4 self bg-(--status-refund) p-2 rounded-lg rounded-b-none">
+
+                  <div className="flex items-center justify-center gap-1 text-sm">
+                    <span className="text-bold text-background">Sehari</span>
+                    <BookingIcons
+                      name="sun"
+                      className="w-4 h-4 text-(--status-wait)"
+                    />
+                  </div>
+
+                  <div className="flex items-center justify-center gap-1 text-sm">
+                    <span className="text-bold text-background">2 Malam</span>
+                    <BookingIcons
+                      name="moon"
+                      className="w-4 h-4 text-(--status-wait)"
+                    />
+                  </div>
+
+                </div>
+
+                <div className="flex p-2">
+                  <button className="flex items-center gap-1.5 bg-background p-2 border-2 border-(--status-refund) rounded-lg cursor-pointer relative"
+                   onClick={() => handleClick()}
+                  >
+                    <BookingIcons
+                      name="check_in"
+                      className="w-5 h-5 text-(--status-refund)"
+                    />
+                    <span
+                      className="text-sm font-bold opacity-60"
+                    >21 November 2025</span>
+                    <input className="absolute top-0 left-0 -z-10" type="date" id="check_in"
+                    ref={checkInInput}
+                    />
+                  </button>
+                </div>
+
+              </div>
+              <div className="w-full p-3 flex items-center gap-2.5 rounded-2xl rounded-tl-none bg-background">
+                <Image
+                  src={"/icons/ic_search.svg"}
+                  width={30}
+                  height={30}
+                  alt="search"
+                />
+                <input
+                  className="w-full min-h-8 outline-none"
+                  placeholder="Cari Hotel Atau Lokasi Hotel Disini!"
+                  type="text"
+                />
+              </div>
+
+            </div>
+
+            <div className="">
+              <h4>Riwayat Pencarian</h4>
+              <div className="flex flex-wrap">
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+
+
+      </div>
     </>
   );
 }
---
+++ components/Navigation.tsx
@@ -0,0 +1,125 @@
+
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Dispatch, SetStateAction, useState } from "react";
+
+interface NavigationLinkProps {
+    name: string,
+    label: string,
+    href: string,
+}
+
+const navigationLink = [
+    {
+        name: "",
+        label: "Halaman Utama",
+        href: "/",
+    },
+    {
+        name: "hotels",
+        label: "Hotel",
+        href: "/hotels",
+    },
+    {
+        name: "vouchers",
+        label: "Kupon & Diskon",
+        href: "/vouchers",
+    },
+    {
+        name: "faqs",
+        label: "Bantuan & Dukungan",
+        href: "/faqs",
+    },
+]
+
+export default function Navigation() {
+
+    const pathName = usePathname()
+
+    const [showNavigation, setShowNavigation] = useState<boolean>(true)
+
+    return <div className="flex justify-between items-center">
+        
+        {showNavigation ? <SideNaviagtionButtonHidden/> : <SideNavigationButton iconName="bermalam" setshow={setShowNavigation} value={true}  />}
+
+        {!showNavigation ? <></> : <div className="w-full max-w-225 h-11 p-2.5 px-5 flex justify-between items-center gap-2.5 rounded-2xl bg-background">
+            <Link
+                href={"/"}
+                className="flex h-full justify-center items-center gap-2"
+            >
+                <Image
+                    src={"/icons/bermalam.svg"}
+                    className="rounded-full"
+                    height={25}
+                    width={25}
+                    alt="bermalam -logo"
+                />
+                <span className="text-sm opacity-80">Bermalam</span>
+            </Link>
+            <div className="w-full flex justify-center items-center gap-4">
+                {
+                    navigationLink.map((nav: NavigationLinkProps, index) => {
+                        return <Link
+                            key={`${nav.name}-${index}`}
+                            href={nav.href}
+                            className={pathName == "/" + nav.name ? "text-(--status-refund)" : ""}
+                        >{nav.label}</Link>
+                    })
+                }
+            </div>
+            <button className="flex justify-center items-center"
+                onClick={() => setShowNavigation(false)}
+            >
+                <Image
+                    src={"/icons/ic_close_tight.svg"}
+                    className="rounded-full"
+                    height={25}
+                    width={25}
+                    alt="ic-close"
+                />
+            </button>
+        </div>
+        }
+        {/* settings */}
+        <div className="w-11 h-11 flex justify-center items-center rounded-full bg-background">
+            <Image
+                src={"/icons/ic_setting.svg"}
+                className="rounded-full"
+                height={30}
+                width={30}
+                alt="icon-setting"
+            />
+        </div>
+    </div>
+}
+
+function SideNaviagtionButtonHidden(){
+    return <div className="w-11 h-11"></div>
+}
+
+function SideNavigationButton(
+    {
+        value,
+        iconName,
+        setshow,
+    } : {
+        value : boolean,
+        iconName: string,
+        setshow: Dispatch<SetStateAction<boolean>>
+    }
+) {
+    return <button
+        className="w-11 h-11 flex justify-center items-center rounded-full bg-background"
+        onClick={() => setshow(value)}
+    >
+        <Image
+            src={`icons/${iconName}.svg`}
+            className="rounded-full"
+            height={25}
+            width={25}
+            alt="bermalam -logo"
+        />
+    </button>
+}
